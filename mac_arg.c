7b82413c893285e5a96b89bf4d228569674e4887
yaoruiming <ruiming.yao>
2019-12-13 23:09:03 +0800

    add mac_arg

---
+++ mac_arg.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define SQUARE(X) X*X
+#define PR(X) printf("The result is %d.\n",X)
+int main(void)
+{
+    int x = 5;
+    int z;
+
+    printf("x = %d\n",x);
+    z = SQUARE(x);
+    printf("Evalu")
+}
